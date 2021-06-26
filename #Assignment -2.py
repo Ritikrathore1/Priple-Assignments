242bb1e0ef673393659a82b6c95fe74037972c4e
RITIK <86469310+Ritikrathore1@users.noreply.github.com>
2021-06-26 23:43:13 +0530

    Add files via upload

---
+++ #Assignment -2.py
@@ -0,0 +1,64 @@
+#Assignment -2
+#Topic:Functions
+
+
+
+def song():
+    print("Tera Ghata")
+def Artist():
+    print("Gajendar Verma")
+def year():
+    print(2020)
+def Genre():
+    print("jazz\n")
+
+def output1():
+    song()
+    Artist()
+    year()
+    Genre()
+    
+    
+output1()
+
+#=======================================================
+
+def song():
+    print("Satisfaya")
+def Artist():
+    print("Imran khan")
+def year():
+    print(2015)
+def Genre():
+    print("jazz\n")
+
+
+def output2():
+    song()
+    Artist()
+    year()
+    Genre()
+   
+    
+output2()
+
+#========================================================
+
+def song():
+    print("bholenath")
+def Artist():
+    print("kaka")
+def year():
+    print(2020)
+def Genre():
+    print("jazz\n")
+
+
+def output3():
+    song()
+    Artist()
+    year()
+    Genre()
+    
+    
+output3()
