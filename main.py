c79e24010f0722268c745a438640a2ce07c848b5
RITIK <86469310+Ritikrathore1@users.noreply.github.com>
2021-06-26 23:42:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+#My spotify favourie song lists 
+#here code starts 
+Song = "Talja"
+Artist = "Gur Sidhu"
+Genre = "Punjabi Pop"
+Durationinseconds = 70
+#From here we will get our output
+print(Song)
+print(Artist)
+print(Genre)
+print(Durationinseconds)
+
+#second song starts
+Song = "kyun faya kun"
+Artist = "AR Rahman"
+Genre = "jazz"
+Durationinseconds = 120
+#From here we will get our output
+print(Song)
+print(Artist)
+print(Genre)
+print(Durationinseconds)
+
+#our third song
+Song = "Minimum"
+Artist = "Russian singer"
+Genre = "Hip hop"
+Durationinseconds = 95
+#From here we will get our output
+print(Song)
+print(Artist)
+print(Genre)
+print(Durationinseconds)
